ddf4a7c20a7620deb2386743903c05a03e80c293
manuahree <manushreeo61@gmail.com>
2023-05-18 21:49:18 +0545

    add weather app script

--- app.js
+++ app.js
@@ -1,15 +1,41 @@
-document.getElementById("getWeatherBtn").addEventListener("click", function() {
-    var city = document.getElementById("cityInput").value;
-    var xhr = new XMLHttpRequest();
-    xhr.open("GET", "weather.php?city=" + city, true);
-    xhr.onload = function() {
-      if (xhr.status === 200) {
-        var weatherData = JSON.parse(xhr.responseText);
-        document.getElementById("weatherInfo").innerHTML = weatherData.description;
-      } else {
-        alert("Error fetching weather data. Please try again.");
-      }
-    };
-    xhr.send();
-  });
-  
+const apiKey = '82dfce36194f8b6cdc2af62c7001b30f';
+const assignedCity = 'Luton';
+
+const searchInput = document.querySelector('.search-input');
+const searchBtn = document.querySelector('.search-btn');
+const city = document.querySelector('.city');
+const temperature = document.querySelector('.temperature');
+const weather = document.querySelector('.weather');
+const wind = document.querySelector('.wind');
+const humidity = document.querySelector('.humidity');
+
+const updateWeather = (data) => {
+  city.textContent = data.name;
+  temperature.textContent = `${Math.round(data.main.temp)}°C`;
+  weather.textContent = data.weather[0].description;
+  wind.textContent = `Wind: ${data.wind.speed}m/s`;
+  humidity.textContent = `Humidity: ${data.main.humidity}%`;
+};
+
+const getWeatherData = async (query) => {
+  try {
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${query}&units=metric&appid=${apiKey}`);
+    const data = await response.json();
+    updateWeather(data);
+  } catch (error) {
+    console.error(error);
+    alert('Error fetching weather data. Please try again later.');
+  }
+};
+
+searchBtn.addEventListener('click', () => {
+  const query = searchInput.value;
+  if (query) {
+    getWeatherData(query);
+    searchInput.value = '';
+  } else {
+    alert('Please enter a city name.');
+  }
+});
+
+getWeatherData('Luton');
